38da2df122c9b3cb2fc31178c7d2a0c0d82e1d88
Csaba44 <csabi16sheldon@gmail.com>
2025-10-20 12:48:18 +0200

    Fix: added missing role_user table migration

--- hemsva_api/routes/api.php
+++ hemsva_api/routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return $request->user()->load('roles');
 })->middleware('auth:sanctum');
 
 Route::post('register', [UserAuthController::class, 'register']);
