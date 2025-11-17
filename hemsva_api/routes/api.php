39a6d152870c89c57f8ec43a8828a6488d9cac02
mezodani <mezodani06@gmail.com>
2025-11-17 12:23:53 +0100

    Fix: Deleted migrations

--- hemsva_api/routes/api.php
+++ hemsva_api/routes/api.php
@@ -9,17 +9,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user()->load('roles');
+    return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('register', [UserAuthController::class, 'register']);
 Route::post('login', [UserAuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [UserAuthController::class, 'logout']);
-    Route::get('/aircraft', [AircraftController::class, 'index']);
-
-
     Route::put('/logger/location', [AircraftLocationController::class, 'update']);
     Route::delete('/logger/location', [AircraftLocationController::class, 'disconnect']);
+
 });
